8ba1cb910be3287fa8387ad466724fa57315ed8f
Gilles De Mey <gilles.de.mey@gmail.com>
2015-12-03 03:30:41 +0100

    Added Pragma (HTTP 1.0) and Expires header

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ module.exports = function() {
 };
 function noCache(req, res, next) {
   res.setHeader('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', 0);
   next();
 }
